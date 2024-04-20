2a8095fa9d5e7c23d6dad4679c1bc53c6d283979
saddam Husain <46366240+saddamhusain92@users.noreply.github.com>
2024-04-20 20:35:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-app.get('/',(res,req)=>{
+app.get('/',(req,res)=>{
     res.send({
         "Status":200
     })
